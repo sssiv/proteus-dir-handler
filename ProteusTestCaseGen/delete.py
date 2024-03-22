45a6122888b3c8851c75ce0cfc491c1b148c1b85
sssiv <Sam.S.Skidmore@gmail.com>
2024-03-21 20:15:44 -0700

    Logging implimented

--- ProteusTestCaseGen/delete.py
+++ ProteusTestCaseGen/delete.py
@@ -1,10 +1,11 @@
 import os
 import shutil
-from paths import *
+from paths import Paths
 
 class Delete():
+    # Helper function, delete stuff in just one folder
     @staticmethod
-    def clear_folder(folder):
+    def __clear_folder(folder):
         for file in os.listdir(folder):
             path = os.path.join(folder, file)
 
@@ -17,12 +18,14 @@ class Delete():
             except Exception as e:
                 print(f"Failed to delete {path} because {e}\n")
     
-    del_successes = staticmethod(lambda: Delete.clear_folder(os.path.join(ROOT, SESSIONS, "Successes")))
-    del_failures = staticmethod(lambda: Delete.clear_folder(os.path.join(ROOT, SESSIONS, "Failures")))
-    del_tests = staticmethod(lambda: Delete.clear_folder(os.path.join(ROOT, SESSIONS, "GeneratedTests")))
-    del_logs = staticmethod(lambda: Delete.clear_folder(os.path.join(ROOT, SAVED, "Logs")))
-    del_sessions = staticmethod(lambda: Delete.clear_folder(os.path.join(ROOT, SAVED, "SavedSessions")))
+    # Delete content in specific folder
+    del_successes = staticmethod(lambda: Delete.__clear_folder(Paths.SUCCESS_PATH))
+    del_failures = staticmethod(lambda: Delete.__clear_folder(Paths.FAIL_PATH))
+    del_tests = staticmethod(lambda: Delete.__clear_folder(Paths.GENERATED_PATH))
+    del_logs = staticmethod(lambda: Delete.__clear_folder(Paths.LOG_PATH))
+    del_sessions = staticmethod(lambda: Delete.__clear_folder(Paths.SESSION_PATH))
 
+    # Delete content in just Successes, Failures, and GeneratedTests
     @staticmethod
     def new_session():
         Delete.del_successes()
@@ -34,5 +37,3 @@ class Delete():
         Delete.new_session()
         Delete.del_logs()
         Delete.del_sessions()
-
-Delete.clear_all()
--- ProteusTestCaseGen/paths.py
+++ ProteusTestCaseGen/paths.py
@@ -1,16 +1,26 @@
 import os
 
+# Want to change the name of your folders? Do it in this file
 def define_root():
+    # User is using windows
     if os.name == "nt":
         return "..\\ProteusTestCaseGen"
+    # User is using MacOS or Linux
     else:
         return "../ProteusTestCaseGen"
     
 ROOT = define_root()
-SAVED = "Saved"
-LOGS = "Logs"
-SAVED_TESTS = "SavedSessions"
-SESSIONS = "TestingSession"
-FAIL = "Failures"
-SUCCESS = "Successes"
-GENERATED = "GemeratedTests"
+SAVED = "Saved"                 
+LOGS = "Logs"                   
+SAVED_SESH = "SavedSessions"    
+SESSIONS = "TestingSession"     
+FAILURE = "Failures"            
+SUCCESS = "Successes"           
+GENERATED = "GeneratedTests"    
+
+class Paths():
+    FAIL_PATH = os.path.join(ROOT, SESSIONS, FAILURE)
+    SUCCESS_PATH = os.path.join(ROOT, SESSIONS, SUCCESS)
+    GENERATED_PATH = os.path.join(ROOT, SESSIONS, GENERATED)
+    LOG_PATH = os.path.join(ROOT, SAVED, LOGS)
+    SESSION_PATH = os.path.join(ROOT, SAVED, SAVED_SESH)
